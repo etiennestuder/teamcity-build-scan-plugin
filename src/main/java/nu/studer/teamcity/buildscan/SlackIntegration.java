d363d1d2a5e248fa9515e3e817d282a172d7fa1e
etiennestuder <etienne@studer.nu>
2016-12-04 19:42:57 +1000

    Render all created build scans in the 'attachments' section of the payload

--- src/main/java/nu/studer/teamcity/buildscan/SlackIntegration.java
+++ src/main/java/nu/studer/teamcity/buildscan/SlackIntegration.java
@@ -60,15 +60,27 @@ final class SlackIntegration {
 
     @NotNull
     static SlackPayload createPayload(@NotNull BuildScanReferences buildScans) {
-        return new SlackPayload()
-            .text("<" + buildScans.first().getUrl() + "|Build scan> published.")
-            .attachment(new SlackPayload.Attachment()
-                .fallback(String.format("Build scan %s published.", buildScans.first().getUrl()))
+        SlackPayload payload = new SlackPayload();
+
+        // main text, only hyper-linking to the build scan if there is only one build scan
+        if (buildScans.size() == 1) {
+            payload.text(String.format("<%s|Build scan> published.", buildScans.first().getUrl()));
+        } else {
+            payload.text(String.format("%d build scans published.", buildScans.size()));
+        }
+
+        // for each build scan, add a separate attachment
+        for (BuildScanReference buildScan : buildScans.all()) {
+            payload.attachment(new SlackPayload.Attachment()
+                .fallback(String.format("Build scan link %s", buildScan.getUrl()))
                 .color("#000000")
                 .field(new SlackPayload.Attachment.Field()
                     .title("Build scan link")
-                    .value(buildScans.first().getUrl())
+                    .value(buildScan.getUrl())
                     .isShort(true)));
+        }
+
+        return payload;
     }
 
     @NotNull
--- src/test/groovy/nu/studer/teamcity/buildscan/SlackIntegrationTest.groovy
+++ src/test/groovy/nu/studer/teamcity/buildscan/SlackIntegrationTest.groovy
@@ -4,9 +4,9 @@ import spock.lang.Specification
 
 class SlackIntegrationTest extends Specification {
 
-    def "properly converts build scan to payload"() {
+    def "properly converts single build scan to payload"() {
         given:
-        def buildScanReferences = BuildScanReferences.of(new BuildScanReference("myId", "http://www.myUrl.org"))
+        def buildScanReferences = BuildScanReferences.of(new BuildScanReference("myId", "http://www.myUrl.org/s/abcde"))
 
         when:
         def payload = SlackIntegration.createPayload(buildScanReferences)
@@ -14,16 +14,57 @@ class SlackIntegrationTest extends Specification {
 
         then:
         json == """{
-  "text": "<http://www.myUrl.org|Build scan> published.",
+  "text": "<http://www.myUrl.org/s/abcde|Build scan> published.",
   "attachments": [
     {
-      "fallback": "Build scan link http://www.myUrl.org",
+      "fallback": "Build scan link http://www.myUrl.org/s/abcde",
       "color": "#000000",
       "fields": [
         {
           "title": "Build scan link",
-          "value": "http://www.myUrl.org",
-          "short": false
+          "value": "http://www.myUrl.org/s/abcde",
+          "short": true
+        }
+      ]
+    }
+  ]
+}"""
+    }
+
+    def "properly converts multiple build scans to payload"() {
+        given:
+        def buildScanReferences = BuildScanReferences.of([
+            new BuildScanReference("myId", "http://www.myUrl.org/s/abcde"),
+            new BuildScanReference("myOtherId", "http://www.myOtherUrl.org/efghi")
+        ])
+
+        when:
+        def payload = SlackIntegration.createPayload(buildScanReferences)
+        def json = SlackPayloadSerializer.create().toJson(payload)
+
+        then:
+        json == """{
+  "text": "2 build scans published.",
+  "attachments": [
+    {
+      "fallback": "Build scan link http://www.myUrl.org/s/abcde",
+      "color": "#000000",
+      "fields": [
+        {
+          "title": "Build scan link",
+          "value": "http://www.myUrl.org/s/abcde",
+          "short": true
+        }
+      ]
+    },
+    {
+      "fallback": "Build scan link http://www.myOtherUrl.org/efghi",
+      "color": "#000000",
+      "fields": [
+        {
+          "title": "Build scan link",
+          "value": "http://www.myOtherUrl.org/efghi",
+          "short": true
         }
       ]
     }
