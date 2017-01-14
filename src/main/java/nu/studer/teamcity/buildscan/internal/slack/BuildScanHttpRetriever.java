1457ab4c3e4209bf0c217f498bdb0b2081f0a714
etiennestuder <etienne@studer.nu>
2017-01-14 05:55:41 +0100

    Retry if scan data retrieval fails

--- src/main/java/nu/studer/teamcity/buildscan/internal/slack/BuildScanHttpRetriever.java
+++ src/main/java/nu/studer/teamcity/buildscan/internal/slack/BuildScanHttpRetriever.java
@@ -12,10 +12,15 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
 import java.nio.charset.StandardCharsets;
+import java.util.concurrent.TimeUnit;
 
 final class BuildScanHttpRetriever {
 
     private static final Logger LOGGER = Logger.getLogger("jetbrains.buildServer.BUILDSCAN");
+
+    // giving up after ~5 minutes of trying
+    private static final int ATTEMPTS_MAX = 7;
+    private static final int RETRY_DELAY_BASELINE_SECS = 15;
 
     private final URL scanUrl;
     private final PasswordCredentials credentials;
@@ -34,6 +39,28 @@ final class BuildScanHttpRetriever {
 
     @NotNull
     BuildScanPayload retrieve() throws IOException {
+        int attempt = 0;
+
+        while (true) {
+            try {
+                return doRetrieve();
+            } catch (IOException e) {
+                LOGGER.error(String.format("Failed to retrieve build scan %s: %s", scanUrl, e.getMessage()));
+
+                // back-off or stop trying
+                if (++attempt < ATTEMPTS_MAX) {
+                    sleepSeconds(RETRY_DELAY_BASELINE_SECS * attempt); // this is not great since we "block" the thread while waiting (even though it runs on a separate execution)
+                } else {
+                    break;
+                }
+            }
+        }
+
+        throw new RuntimeException("Unable to retrieved build scan at " + scanUrl);
+    }
+
+    @NotNull
+    private BuildScanPayload doRetrieve() throws IOException {
         URLConnection urlConnection = scanUrl.openConnection();
         if (!(urlConnection instanceof HttpURLConnection)) {
             throw new IllegalArgumentException("HttpURLConnection expected");
@@ -65,6 +92,14 @@ final class BuildScanHttpRetriever {
         LOGGER.info("Invoking build scan data end-point returned response code: " + responseCode);
 
         return buildScanPayload;
+    }
+
+    private void sleepSeconds(int seconds) {
+        try {
+            TimeUnit.SECONDS.sleep(seconds);
+        } catch (InterruptedException e) {
+            // ignore
+        }
     }
 
 }
