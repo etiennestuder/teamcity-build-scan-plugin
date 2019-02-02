2bd75c7f8b455810ed72ba9a6533099cc2436cb3
Etienne Studer <etienne@studer.nu>
2019-02-02 16:20:38 +0100

    Reduce class and method visibility

--- src/main/java/nu/studer/teamcity/buildscan/internal/Util.java
+++ src/main/java/nu/studer/teamcity/buildscan/internal/Util.java
@@ -4,18 +4,15 @@ import java.util.Optional;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public final class Util {
+final class Util {
 
     private static final Pattern BUILD_SCAN_URL_PATTERN = Pattern.compile("https?://.*/s/(.*)");
 
-    private Util() {
-    }
-
-    public static boolean isBuildScanUrl(String text) {
+    static boolean isBuildScanUrl(String text) {
         return doGetBuildScanId(text).isPresent();
     }
 
-    public static String getBuildScanId(String text) {
+    static String getBuildScanId(String text) {
         //noinspection OptionalGetWithoutIsPresent
         return doGetBuildScanId(text).get();
     }
@@ -24,4 +21,8 @@ public final class Util {
         Matcher matcher = BUILD_SCAN_URL_PATTERN.matcher(text);
         return matcher.matches() ? Optional.of(matcher.group(1)) : Optional.empty();
     }
+
+    private Util() {
+    }
+
 }
