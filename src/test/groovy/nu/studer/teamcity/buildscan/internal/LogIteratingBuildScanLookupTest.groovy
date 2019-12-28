f78570c8c137228d32a20832842195200a3d98ef
Etienne Studer <etienne@studer.nu>
2019-12-28 17:07:47 +0100

    Reorder test methods

--- src/test/groovy/nu/studer/teamcity/buildscan/internal/LogIteratingBuildScanLookupTest.groovy
+++ src/test/groovy/nu/studer/teamcity/buildscan/internal/LogIteratingBuildScanLookupTest.groovy
@@ -10,6 +10,50 @@ import spock.lang.Unroll
 
 class LogIteratingBuildScanLookupTest extends Specification {
 
+    def "avoids iterating build log when disabled via configuration"() {
+        given:
+        LogIteratingBuildScanLookup lookup = new LogIteratingBuildScanLookup()
+
+        and:
+        BuildLog buildLog = Mock(BuildLog)
+        ParametersProvider parametersProvider = Mock(ParametersProvider) {
+            get("BUILD_SCAN_LOG_PARSING") >> 'false'
+        }
+
+        SBuild build = Mock(SBuild) {
+            getBuildLog() >> buildLog
+            getParametersProvider() >> parametersProvider
+        }
+
+        when:
+        lookup.getBuildScansForBuild(build)
+
+        then:
+        0 * build.isFinished()
+        0 * buildLog.getMessagesIterator()
+    }
+
+    def "avoids iterating build log when build is still running"() {
+        given:
+        LogIteratingBuildScanLookup lookup = new LogIteratingBuildScanLookup()
+
+        and:
+        BuildLog buildLog = Mock(BuildLog)
+        ParametersProvider parametersProvider = Mock(ParametersProvider)
+
+        SBuild build = Mock(SBuild) {
+            getBuildLog() >> buildLog
+            getParametersProvider() >> parametersProvider
+            isFinished() >> false
+        }
+
+        when:
+        lookup.getBuildScansForBuild(build)
+
+        then:
+        0 * buildLog.getMessagesIterator()
+    }
+
     @Unroll
     def "finds one or more build scan publications if preceded by publishing message"() {
         given:
@@ -17,6 +61,7 @@ class LogIteratingBuildScanLookupTest extends Specification {
 
         and:
         SBuild build = Mock(SBuild) {
+            getParametersProvider() >> Mock(ParametersProvider)
             isFinished() >> true
         }
         BuildLog buildLog = Mock(BuildLog)
@@ -82,51 +127,6 @@ class LogIteratingBuildScanLookupTest extends Specification {
             log("Publishing build scan..."),
             log("some text"),
             log("https://scans.grdev.net/s/fgb5fkqivexry")]       | []
-    }
-
-    def "avoids iterating build log when build is still running"() {
-        given:
-        LogIteratingBuildScanLookup lookup = new LogIteratingBuildScanLookup()
-
-        and:
-        BuildLog buildLog = Mock(BuildLog)
-        ParametersProvider parametersProvider = Mock(ParametersProvider)
-
-        SBuild build = Mock(SBuild) {
-            getBuildLog() >> buildLog
-            getParametersProvider() >> parametersProvider
-            isFinished() >> false
-        }
-
-        when:
-        lookup.getBuildScansForBuild(build)
-
-        then:
-        0 * buildLog.getMessagesIterator()
-    }
-
-    def "avoids iterating build log when disabled via configuration"() {
-        given:
-        LogIteratingBuildScanLookup lookup = new LogIteratingBuildScanLookup()
-
-        and:
-        BuildLog buildLog = Mock(BuildLog)
-        ParametersProvider parametersProvider = Mock(ParametersProvider) {
-            get("BUILD_SCAN_LOG_PARSING") >> 'false'
-        }
-
-        SBuild build = Mock(SBuild) {
-            getBuildLog() >> buildLog
-            getParametersProvider() >> parametersProvider
-            isFinished() >> true
-        }
-
-        when:
-        lookup.getBuildScansForBuild(build)
-
-        then:
-        0 * build.isFinished()
-        0 * buildLog.getMessagesIterator()
     }
 
     private static LogMessage log(String text) {
