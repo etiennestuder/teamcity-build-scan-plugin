a0b9a43ebdfa524eba1e1209ab8a149d489d8c7e
Etienne Studer <etienne@studer.nu>
2020-04-18 23:53:42 +0200

    Throw exception when writing to the custom data storage is requested

--- src/main/java/nu/studer/teamcity/buildscan/internal/CustomDataStorageBuildScanDataStore.java
+++ src/main/java/nu/studer/teamcity/buildscan/internal/CustomDataStorageBuildScanDataStore.java
@@ -5,50 +5,26 @@ import jetbrains.buildServer.serverSide.SBuild;
 import jetbrains.buildServer.util.StringUtil;
 import nu.studer.teamcity.buildscan.BuildScanDataStore;
 import nu.studer.teamcity.buildscan.BuildScanReference;
-import org.apache.log4j.Logger;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static nu.studer.teamcity.buildscan.internal.Util.getBuildScanId;
 
 public final class CustomDataStorageBuildScanDataStore implements BuildScanDataStore {
 
     private static final String BUILD_SCAN_STORAGE_ID = "nu.studer.teamcity.buildscan.DefaultBuildScanDataStore";
     private static final String BUILD_SCAN_URLS_SEPARATOR = "|";
 
-    private static final Logger LOGGER = Logger.getLogger("jetbrains.buildServer.BUILDSCAN");
-
     @Override
     public void mark(SBuild build) {
-        String buildId = String.valueOf(build.getBuildId());
-        CustomDataStorage customDataStorage = getCustomDataStorage(build);
-        String existing = customDataStorage.getValue(buildId);
-
-        if (existing == null) {
-            customDataStorage.putValue(buildId, "");
-            customDataStorage.flush();
-        }
+        throw new UnsupportedOperationException("Writing to the custom data storage is not supported anymore");
     }
 
     @Override
     public void store(SBuild build, String buildScanUrl) {
-        String buildId = String.valueOf(build.getBuildId());
-        CustomDataStorage customDataStorage = getCustomDataStorage(build);
-        String existing = customDataStorage.getValue(buildId);
-
-        if (existing == null || existing.isEmpty()) {
-            LOGGER.info("Storing build scan URL: " + buildScanUrl + ", for build id: " + buildId);
-            customDataStorage.putValue(buildId, buildScanUrl);
-            customDataStorage.flush();
-        } else {
-            List<String> scans = StringUtil.split(existing, BUILD_SCAN_URLS_SEPARATOR);
-            scans.add(buildScanUrl);
-
-            String buildScanString = StringUtil.join(BUILD_SCAN_URLS_SEPARATOR, scans);
-            LOGGER.info("Storing build scan URLs: " + buildScanString + ", for build id: " + buildId);
-            customDataStorage.putValue(buildId, buildScanString);
-            customDataStorage.flush();
-        }
+        throw new UnsupportedOperationException("Writing to the custom data storage is not supported anymore");
     }
 
     @Override
@@ -64,7 +40,7 @@ public final class CustomDataStorageBuildScanDataStore implements BuildScanDataS
             List<String> scans = StringUtil.split(existing, BUILD_SCAN_URLS_SEPARATOR);
             for (String scanUrl : scans) {
                 if (!scanUrl.isEmpty()) {
-                    buildScanReferences.add(new BuildScanReference(Util.getBuildScanId(scanUrl), scanUrl));
+                    buildScanReferences.add(new BuildScanReference(getBuildScanId(scanUrl), scanUrl));
                 }
             }
 
