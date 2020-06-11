5fe55c21215685f95f192ff0afe67662d2d287e7
Etienne Studer <etienne@studer.nu>
2020-06-11 11:42:32 +0200

    Avoid dependency on slf4j

--- agent/service-message-maven-extension/src/main/java/nu/studer/teamcity/buildscan/agent/servicemessage/BuildScanServiceMessageMavenExtension.java
+++ agent/service-message-maven-extension/src/main/java/nu/studer/teamcity/buildscan/agent/servicemessage/BuildScanServiceMessageMavenExtension.java
@@ -6,31 +6,30 @@ import org.apache.maven.MavenExecutionException;
 import org.apache.maven.execution.MavenSession;
 import org.codehaus.plexus.PlexusContainer;
 import org.codehaus.plexus.component.annotations.Component;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.codehaus.plexus.logging.Logger;
 
 import javax.inject.Inject;
 
 @Component(role = AbstractMavenLifecycleParticipant.class)
 public final class BuildScanServiceMessageMavenExtension extends AbstractMavenLifecycleParticipant {
 
-    private static final Logger LOG = LoggerFactory.getLogger(BuildScanServiceMessageMavenExtension.class);
-
     private final PlexusContainer container;
+    private final Logger logger;
 
     @Inject
-    public BuildScanServiceMessageMavenExtension(PlexusContainer container) {
+    public BuildScanServiceMessageMavenExtension(PlexusContainer container, Logger logger) {
         this.container = container;
+        this.logger = logger;
     }
 
     @Override
     public void afterProjectsRead(MavenSession session) throws MavenExecutionException {
-        LOG.debug("Executing extension: {}", getClass().getSimpleName());
+        logger.debug("Executing extension: " + getClass().getSimpleName());
         BuildScanApi buildScan = BuildScanApiAccessor.lookup(container, getClass());
         if (buildScan != null) {
-            LOG.debug("Registering listener capturing build scan link");
+            logger.debug("Registering listener capturing build scan link");
             BuildScanServiceMessageSender.register(buildScan);
-            LOG.debug("Finished registering listener capturing build scan link");
+            logger.debug("Finished registering listener capturing build scan link");
         }
     }
 
