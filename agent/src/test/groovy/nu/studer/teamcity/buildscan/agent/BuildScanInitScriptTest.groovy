72bcf69ce5a529965d89ca681ce67751498b271a
Etienne Studer <etienne@studer.nu>
2020-10-16 12:23:58 +0200

    Ensure tests run with a valid permutation of JDK version and Gradle version

--- agent/src/test/groovy/nu/studer/teamcity/buildscan/agent/BuildScanInitScriptTest.groovy
+++ agent/src/test/groovy/nu/studer/teamcity/buildscan/agent/BuildScanInitScriptTest.groovy
@@ -2,58 +2,94 @@ package nu.studer.teamcity.buildscan.agent
 
 import org.gradle.util.GradleVersion
 
+import static org.junit.Assume.assumeTrue
+
 class BuildScanInitScriptTest extends BaseInitScriptTest {
 
-    private static final List<GradleVersion> NOT_SUPPORTED_GRADLE_VERSIONS = [
-        GradleVersion.version('3.5.1'),
-        GradleVersion.version('4.0.2')
+    private static final List<JdkCompatibleGradleVersion> NOT_SUPPORTED_GRADLE_VERSIONS = [
+        new JdkCompatibleGradleVersion(GradleVersion.version('3.5.1'), 7, 9),
+        new JdkCompatibleGradleVersion(GradleVersion.version('4.0.2'), 7, 9)
     ]
 
-    private static final List<GradleVersion> SUPPORTED_GRADLE_VERSIONS = [
-        GradleVersion.version('4.1'),
-        GradleVersion.version('4.10.3'),
-        GradleVersion.version('5.1.1'),
-        GradleVersion.version('5.6.4'),
-        GradleVersion.version('6.0.1'),
-        GradleVersion.current()
+    private static final List<JdkCompatibleGradleVersion> SUPPORTED_GRADLE_VERSIONS = [
+        new JdkCompatibleGradleVersion(GradleVersion.version('4.1'), 7, 9),
+        new JdkCompatibleGradleVersion(GradleVersion.version('4.10.3'), 7, 10),
+        new JdkCompatibleGradleVersion(GradleVersion.version('5.1.1'), 8, 11),
+        new JdkCompatibleGradleVersion(GradleVersion.version('5.6.4'), 8, 12),
+        new JdkCompatibleGradleVersion(GradleVersion.version('6.0.1'), 8, 13),
+        new JdkCompatibleGradleVersion(GradleVersion.version('6.7'), 8, 15)
     ]
 
-    def "does not fail build when using a Gradle version older than minimum version 4.1 (#gradleVersion)"() {
+    def "does not fail build when using a Gradle version older than minimum version 4.1 (#jdkCompatibleGradleVersion)"() {
+        assumeTrue jdkCompatibleGradleVersion.isJvmVersionCompatible()
+
         when:
-        def result = run(gradleVersion)
+        def result = run(jdkCompatibleGradleVersion.gradleVersion)
 
         then:
         result
 
         where:
-        gradleVersion << NOT_SUPPORTED_GRADLE_VERSIONS
+        jdkCompatibleGradleVersion << NOT_SUPPORTED_GRADLE_VERSIONS
     }
 
-    def "sends build started service message even without declaring Build Scan / Gradle Enterprise plugin (#gradleVersion)"() {
+    def "sends build started service message even without declaring Build Scan / Gradle Enterprise plugin (#jdkCompatibleGradleVersion)"() {
+        assumeTrue jdkCompatibleGradleVersion.isJvmVersionCompatible()
+
         when:
-        def result = run(gradleVersion)
+        def result = run(jdkCompatibleGradleVersion.gradleVersion)
 
         then:
         outputContainsTeamCityServiceMessageBuildStarted(result)
 
         where:
-        gradleVersion << SUPPORTED_GRADLE_VERSIONS
+        jdkCompatibleGradleVersion << SUPPORTED_GRADLE_VERSIONS
     }
 
-    def "send build scan url service message when declaring Build Scan / Gradle Enterprise plugin (#gradleVersion)"() {
+    def "send build scan url service message when declaring Build Scan / Gradle Enterprise plugin (#jdkCompatibleGradleVersion)"() {
+        assumeTrue jdkCompatibleGradleVersion.isJvmVersionCompatible()
+
         given:
-        settingsFile << maybeAddGradleEnterprisePlugin(gradleVersion)
-        buildFile << maybeAddBuildScanPlugin(gradleVersion)
+        settingsFile << maybeAddGradleEnterprisePlugin(jdkCompatibleGradleVersion.gradleVersion)
+        buildFile << maybeAddBuildScanPlugin(jdkCompatibleGradleVersion.gradleVersion)
 
         when:
-        def result = run(gradleVersion)
+        def result = run(jdkCompatibleGradleVersion.gradleVersion)
 
         then:
         outputContainsTeamCityServiceMessageBuildStarted(result)
         outputContainsTeamCityServiceMessageBuildScanUrl(result)
 
         where:
-        gradleVersion << SUPPORTED_GRADLE_VERSIONS
+        jdkCompatibleGradleVersion << SUPPORTED_GRADLE_VERSIONS
+    }
+
+    static final class JdkCompatibleGradleVersion {
+
+        private final GradleVersion gradleVersion
+        private final Integer jdkMin
+        private final Integer jdkMax
+
+        JdkCompatibleGradleVersion(GradleVersion gradleVersion, Integer jdkMin, Integer jdkMax) {
+            this.gradleVersion = gradleVersion
+            this.jdkMin = jdkMin
+            this.jdkMax = jdkMax
+        }
+
+        boolean isJvmVersionCompatible() {
+            def jvmVersion = getJvmVersion()
+            jdkMin <= jvmVersion && jvmVersion <= jdkMax
+        }
+
+        private static int getJvmVersion() {
+            String version = System.getProperty('java.version');
+            if (version.startsWith('1.')) {
+                Integer.parseInt(version.substring(2, 3))
+            } else {
+                Integer.parseInt(version.substring(0, version.indexOf('.')))
+            }
+        }
+
     }
 
 }
