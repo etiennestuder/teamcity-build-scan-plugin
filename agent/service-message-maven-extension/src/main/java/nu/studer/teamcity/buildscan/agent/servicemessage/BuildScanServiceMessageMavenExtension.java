5301c239e4f0525d2379f1eeeef7f2e33083d83d
Clay Johnson <cjohnson@gradle.com>
2022-05-27 07:22:43 +0200

    Ignore JetBrains Maven info goal when publishing Build Scans (#65)

--- agent/service-message-maven-extension/src/main/java/nu/studer/teamcity/buildscan/agent/servicemessage/BuildScanServiceMessageMavenExtension.java
+++ agent/service-message-maven-extension/src/main/java/nu/studer/teamcity/buildscan/agent/servicemessage/BuildScanServiceMessageMavenExtension.java
@@ -7,6 +7,8 @@ import org.codehaus.plexus.component.annotations.Component;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.List;
+
 @SuppressWarnings("unused")
 @Component(
     role = GradleEnterpriseListener.class,
@@ -21,9 +23,27 @@ public final class BuildScanServiceMessageMavenExtension implements GradleEnterp
     public void configure(GradleEnterpriseApi api, MavenSession session) {
         logger.debug("Executing extension: " + getClass().getSimpleName());
 
-        logger.debug("Registering listener capturing build scan link");
-        BuildScanServiceMessageSender.register(api.getBuildScan());
-        logger.debug("Finished registering listener capturing build scan link");
+        // When a maven build step has an explicitly defined pom file location, TeamCity will run an info goal on it.
+        // This logic prevents that scan from being published.
+        if (isJetbrainsInfoGoal(api, session)) {
+            String goal = session.getGoals().get(0); // `isJetbrainsInfoGoal` guarantees exactly 1 element
+            logger.debug("Skipping Build Scan publish for goal " + goal);
+            api.getBuildScan().publishOnDemand();
+        } else {
+            logger.debug("Registering listener capturing build scan link");
+            BuildScanServiceMessageSender.register(api.getBuildScan());
+            logger.debug("Finished registering listener capturing build scan link");
+        }
+    }
+
+    private boolean isJetbrainsInfoGoal(GradleEnterpriseApi api, MavenSession session) {
+        List<String> goals = session.getGoals();
+        if (goals.size() == 1) {
+            String goal = goals.get(0);
+            return goal.startsWith("org.jetbrains.maven:info-maven3-plugin:") && goal.endsWith(":info");
+        } else {
+            return false;
+        }
     }
 
 }
