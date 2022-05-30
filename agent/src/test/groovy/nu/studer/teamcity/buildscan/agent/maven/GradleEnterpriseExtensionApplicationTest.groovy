872b1d68336ac25c053a183a5592ae43f2887af0
etiennestuder <etienne@studer.nu>
2022-05-30 16:59:10 +0200

    Add Maven 3.5.x to cross-version testing

--- agent/src/test/groovy/nu/studer/teamcity/buildscan/agent/maven/GradleEnterpriseExtensionApplicationTest.groovy
+++ agent/src/test/groovy/nu/studer/teamcity/buildscan/agent/maven/GradleEnterpriseExtensionApplicationTest.groovy
@@ -13,6 +13,8 @@ import static org.junit.Assume.assumeTrue
 class GradleEnterpriseExtensionApplicationTest extends Specification {
 
     static final List<JdkCompatibleMavenVersion> SUPPORTED_MAVEN_VERSIONS = [
+        new JdkCompatibleMavenVersion('3.5.0', 7, 11),
+        new JdkCompatibleMavenVersion('3.5.4', 7, 11),
         new JdkCompatibleMavenVersion('3.6.0', 7, 11),
         new JdkCompatibleMavenVersion('3.6.3', 7, 11),
         new JdkCompatibleMavenVersion('3.8.1', 7, 11),
