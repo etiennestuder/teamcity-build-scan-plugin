cc394502daffd25f5abcd5a8a0d7d917c72cae6e
etiennestuder <etienne@studer.nu>
2022-06-04 07:35:58 +0200

    Update references to CCUD plugin 1.7.2

--- agent/src/test/groovy/nu/studer/teamcity/buildscan/agent/gradle/GradleEnterprisePluginApplicationTest.groovy
+++ agent/src/test/groovy/nu/studer/teamcity/buildscan/agent/gradle/GradleEnterprisePluginApplicationTest.groovy
@@ -8,7 +8,7 @@ import static org.junit.Assume.assumeTrue
 class GradleEnterprisePluginApplicationTest extends BaseInitScriptTest {
 
     private static final String GE_PLUGIN_VERSION = '3.10.1'
-    private static final String CCUD_PLUGIN_VERSION = '1.7.1'
+    private static final String CCUD_PLUGIN_VERSION = '1.7.2'
 
     private static final GradleVersion GRADLE_6 = GradleVersion.version('6.0')
 
