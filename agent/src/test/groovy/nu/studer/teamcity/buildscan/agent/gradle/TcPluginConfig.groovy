9ddddf1a14431d732647ccb4efbeaf16dcce4c5d
Daz DeBoer <daz@gradle.com>
2022-06-08 07:59:36 -0600

    Reformat Groovy class

--- agent/src/test/groovy/nu/studer/teamcity/buildscan/agent/gradle/TcPluginConfig.groovy
+++ agent/src/test/groovy/nu/studer/teamcity/buildscan/agent/gradle/TcPluginConfig.groovy
@@ -2,50 +2,50 @@ package nu.studer.teamcity.buildscan.agent.gradle
 
 final class TcPluginConfig {
 
-     URI gradlePluginRepositoryUrl
-     URI geUrl
-     boolean geAllowUntrustedServer
-     String gePluginVersion
-     String ccudPluginVersion
+    URI gradlePluginRepositoryUrl
+    URI geUrl
+    boolean geAllowUntrustedServer
+    String gePluginVersion
+    String ccudPluginVersion
 
-     Map<String, String> toConfigProperties() {
-         Map<String, String> configProps = [:]
-         if (gradlePluginRepositoryUrl) {
-             configProps.put 'buildScanPlugin.gradle.plugin-repository.url', gradlePluginRepositoryUrl.toString()
-         }
-         if (geUrl) {
-             configProps.put 'buildScanPlugin.gradle-enterprise.url', geUrl.toString()
-         }
-         if (geAllowUntrustedServer) {
-             configProps.put 'buildScanPlugin.gradle-enterprise.allow-untrusted-server', 'true'
-         }
-         if (gePluginVersion) {
-             configProps.put 'buildScanPlugin.gradle-enterprise.plugin.version', gePluginVersion
-         }
-         if (ccudPluginVersion) {
-             configProps.put 'buildScanPlugin.ccud.plugin.version', ccudPluginVersion
-         }
-         configProps
-     }
+    Map<String, String> toConfigProperties() {
+        Map<String, String> configProps = [:]
+        if (gradlePluginRepositoryUrl) {
+            configProps.put 'buildScanPlugin.gradle.plugin-repository.url', gradlePluginRepositoryUrl.toString()
+        }
+        if (geUrl) {
+            configProps.put 'buildScanPlugin.gradle-enterprise.url', geUrl.toString()
+        }
+        if (geAllowUntrustedServer) {
+            configProps.put 'buildScanPlugin.gradle-enterprise.allow-untrusted-server', 'true'
+        }
+        if (gePluginVersion) {
+            configProps.put 'buildScanPlugin.gradle-enterprise.plugin.version', gePluginVersion
+        }
+        if (ccudPluginVersion) {
+            configProps.put 'buildScanPlugin.ccud.plugin.version', ccudPluginVersion
+        }
+        configProps
+    }
 
-     // Generate sysProps for the init-script directly, for Gradle versions where TestKit doesn't support env vars
-     List<String> toSysProps() {
-         def jvmArgs = ["-DteamCityBuildScanPlugin.init-script-name=build-scan-init.gradle"]
-         if (gradlePluginRepositoryUrl) {
-             jvmArgs << "-DteamCityBuildScanPlugin.gradle.plugin-repository.url=$gradlePluginRepositoryUrl".toString()
-         }
-         if (geUrl) {
-             jvmArgs << "-DteamCityBuildScanPlugin.gradle-enterprise.url=$geUrl".toString()
-         }
-         if (geAllowUntrustedServer) {
-             jvmArgs << "-DteamCityBuildScanPlugin.gradle-enterprise.allow-untrusted-server=true"
-         }
-         if (gePluginVersion) {
-             jvmArgs << "-DteamCityBuildScanPlugin.gradle-enterprise.plugin.version=$gePluginVersion".toString()
-         }
-         if (ccudPluginVersion) {
-             jvmArgs << "-DteamCityBuildScanPlugin.ccud.plugin.version=$ccudPluginVersion".toString()
-         }
-         jvmArgs
-     }
- }
+    // Generate sysProps for the init-script directly, for Gradle versions where TestKit doesn't support env vars
+    List<String> toSysProps() {
+        def jvmArgs = ["-DteamCityBuildScanPlugin.init-script-name=build-scan-init.gradle"]
+        if (gradlePluginRepositoryUrl) {
+            jvmArgs << "-DteamCityBuildScanPlugin.gradle.plugin-repository.url=$gradlePluginRepositoryUrl".toString()
+        }
+        if (geUrl) {
+            jvmArgs << "-DteamCityBuildScanPlugin.gradle-enterprise.url=$geUrl".toString()
+        }
+        if (geAllowUntrustedServer) {
+            jvmArgs << "-DteamCityBuildScanPlugin.gradle-enterprise.allow-untrusted-server=true"
+        }
+        if (gePluginVersion) {
+            jvmArgs << "-DteamCityBuildScanPlugin.gradle-enterprise.plugin.version=$gePluginVersion".toString()
+        }
+        if (ccudPluginVersion) {
+            jvmArgs << "-DteamCityBuildScanPlugin.ccud.plugin.version=$ccudPluginVersion".toString()
+        }
+        jvmArgs
+    }
+}
