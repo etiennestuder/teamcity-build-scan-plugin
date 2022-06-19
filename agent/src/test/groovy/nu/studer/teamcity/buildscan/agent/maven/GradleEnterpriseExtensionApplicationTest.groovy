056d44894d89d4186165d574d6f540cffc2a5fa0
Etienne Studer <etienne@studer.nu>
2022-06-19 09:02:33 -0700

    Bump Maven version in cross-version tests to 3.8.6

--- agent/src/test/groovy/nu/studer/teamcity/buildscan/agent/maven/GradleEnterpriseExtensionApplicationTest.groovy
+++ agent/src/test/groovy/nu/studer/teamcity/buildscan/agent/maven/GradleEnterpriseExtensionApplicationTest.groovy
@@ -19,7 +19,7 @@ class GradleEnterpriseExtensionApplicationTest extends Specification {
         new JdkCompatibleMavenVersion('3.6.0', 7, 11),
         new JdkCompatibleMavenVersion('3.6.3', 7, 11),
         new JdkCompatibleMavenVersion('3.8.1', 7, 11),
-        new JdkCompatibleMavenVersion('3.8.5', 7, 11)
+        new JdkCompatibleMavenVersion('3.8.6', 7, 11)
     ]
 
     static final String GE_URL = System.getenv('GRADLE_ENTERPRISE_TEST_INSTANCE')
