f3295b25f6976f040bf0c5b5bac17347cd142e1d
Etienne Studer <etienne@studer.nu>
2022-07-04 05:12:25 +0200

    Have consistent method order

--- agent/src/test/groovy/nu/studer/teamcity/buildscan/agent/maven/MavenRunner.groovy
+++ agent/src/test/groovy/nu/studer/teamcity/buildscan/agent/maven/MavenRunner.groovy
@@ -5,16 +5,11 @@ final class MavenRunner {
     String version
     File projectDir
     File installationDir
+    File multiModuleProjectDir
     List<String> arguments
-    File multiModuleProjectDirectory
 
     MavenRunner withVersion(String version) {
         this.version = version
-        this
-    }
-
-    MavenRunner withInstallationDirectory(File installationDir) {
-        this.installationDir = installationDir
         this
     }
 
@@ -27,6 +22,16 @@ final class MavenRunner {
         withProjectDir(new File(projectDir))
     }
 
+    MavenRunner withInstallationDirectory(File installationDir) {
+        this.installationDir = installationDir
+        this
+    }
+
+    MavenRunner withMultiModuleProjectDirectory(File multiModuleProjectDirectory) {
+        this.multiModuleProjectDir = multiModuleProjectDirectory
+        this
+    }
+
     MavenRunner withArguments(String... arguments) {
         this.arguments = arguments
         this
@@ -37,47 +42,26 @@ final class MavenRunner {
         this
     }
 
-    MavenRunner withMultiModuleProjectDirectory(File multiModuleProjectDirectory) {
-        this.multiModuleProjectDirectory = multiModuleProjectDirectory
-        this
-    }
-
     String build() {
         checkForRequiredProperties()
         installMaven()
         run()
     }
 
-    private String run() {
-        def mvnExecutableName = System.getProperty('os.name').startsWith('Windows') ? 'mvn.cmd' : 'mvn'
-        def mvn = new File(installationDir, mvnExecutableName)
-
-        // because we are running a maven wrapper from a different directory, we set the multiModuleProjectDirectory to where the .mvn folder is
-        // this is how Maven finds the .mvn folder, but the maven wrapper only finds it relative to the mvn wrapper itself
-        def defaultArgs = ['-B', "-Dmaven.multiModuleProjectDirectory=${multiModuleProjectDirectory}".toString()]
-        def userArgs = arguments.collectMany { s -> s.trim().split(' ').toList() }
-        def command = [mvn.absolutePath] + defaultArgs + userArgs
-
-        new ProcessBuilder(command)
-            .directory(projectDir)
-            .start()
-            .text
-    }
-
     private void checkForRequiredProperties() {
-        if (!installationDir) {
-            throw new IllegalStateException("Maven installation directory is not set")
+        if (!version) {
+            throw new IllegalStateException("Maven version is not set")
         }
 
         if (!projectDir) {
             throw new IllegalStateException("Maven project directory is not set")
         }
 
-        if (!version) {
-            throw new IllegalStateException("Maven version is not set")
+        if (!installationDir) {
+            throw new IllegalStateException("Maven installation directory is not set")
         }
 
-        if (!multiModuleProjectDirectory) {
+        if (!multiModuleProjectDir) {
             throw new IllegalStateException("multiModuleProjectDirectory is not set")
         }
     }
@@ -95,4 +79,21 @@ final class MavenRunner {
 wrapperUrl=https://repo.maven.apache.org/maven2/org/apache/maven/wrapper/maven-wrapper/3.1.0/maven-wrapper-3.1.0.jar
 """
     }
+
+    private String run() {
+        def mvnExecutableName = System.getProperty('os.name').startsWith('Windows') ? 'mvn.cmd' : 'mvn'
+        def mvn = new File(installationDir, mvnExecutableName)
+
+        // because we are running a maven wrapper from a different directory, we set the multiModuleProjectDirectory to where the .mvn folder is
+        // this is how Maven finds the .mvn folder, but the maven wrapper only finds it relative to the mvn wrapper itself
+        def defaultArgs = ['-B', "-Dmaven.multiModuleProjectDirectory=${multiModuleProjectDir}".toString()]
+        def userArgs = arguments.collectMany { s -> s.trim().split(' ').toList() }
+        def command = [mvn.absolutePath] + defaultArgs + userArgs
+
+        new ProcessBuilder(command)
+            .directory(projectDir)
+            .start()
+            .text
+    }
+
 }
