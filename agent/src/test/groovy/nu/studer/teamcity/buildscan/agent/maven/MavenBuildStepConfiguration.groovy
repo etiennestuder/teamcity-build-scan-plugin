7b8d5fd09c3b68d64be20e2c3fdb64212cf82d3f
Clay Johnson <cjohnson@gradle.com>
2022-07-05 14:27:19 -0500

    Fix 5 spacing indentation in `MavenBuildStepConfiguration`

--- agent/src/test/groovy/nu/studer/teamcity/buildscan/agent/maven/MavenBuildStepConfiguration.groovy
+++ agent/src/test/groovy/nu/studer/teamcity/buildscan/agent/maven/MavenBuildStepConfiguration.groovy
@@ -1,49 +1,49 @@
 package nu.studer.teamcity.buildscan.agent.maven
 
 final class MavenBuildStepConfiguration {
-     String goals = 'clean package'
-     String pathToPomFile = null
+    String goals = 'clean package'
+    String pathToPomFile = null
 
-     String geUrl = null
-     String geExtensionVersion = null
-     String ccudExtensionVersion = null
-     String geExtensionCustomCoordinates = null
-     String ccudExtensionCustomCoordinates = null
-     Boolean allowUntrustedServer = null
-     Boolean commandLineBuildStepEnabled = null
+    String geUrl = null
+    String geExtensionVersion = null
+    String ccudExtensionVersion = null
+    String geExtensionCustomCoordinates = null
+    String ccudExtensionCustomCoordinates = null
+    Boolean allowUntrustedServer = null
+    Boolean commandLineBuildStepEnabled = null
 
-     void applyTo(Map<String, String> configParameters, Map<String, String> runnerParameters) {
-         applyRunnerParameters(runnerParameters)
-         applyConfigParameters(configParameters)
-     }
+    void applyTo(Map<String, String> configParameters, Map<String, String> runnerParameters) {
+        applyRunnerParameters(runnerParameters)
+        applyConfigParameters(configParameters)
+    }
 
-     private void applyRunnerParameters(Map<String, String> runnerParameters) {
-         runnerParameters.put('goals', goals)
+    private void applyRunnerParameters(Map<String, String> runnerParameters) {
+        runnerParameters.put('goals', goals)
 
-         if (pathToPomFile)
-             runnerParameters.put('pomLocation', pathToPomFile)
-     }
+        if (pathToPomFile)
+            runnerParameters.put('pomLocation', pathToPomFile)
+    }
 
-     private void applyConfigParameters(Map<String, String> configParameters) {
-         if (geUrl)
-             configParameters.put('buildScanPlugin.gradle-enterprise.url', geUrl)
+    private void applyConfigParameters(Map<String, String> configParameters) {
+        if (geUrl)
+            configParameters.put('buildScanPlugin.gradle-enterprise.url', geUrl)
 
-         if (geExtensionVersion)
-             configParameters.put('buildScanPlugin.gradle-enterprise.extension.version', geExtensionVersion)
+        if (geExtensionVersion)
+            configParameters.put('buildScanPlugin.gradle-enterprise.extension.version', geExtensionVersion)
 
-         if (ccudExtensionVersion)
-             configParameters.put('buildScanPlugin.ccud.extension.version', ccudExtensionVersion)
+        if (ccudExtensionVersion)
+            configParameters.put('buildScanPlugin.ccud.extension.version', ccudExtensionVersion)
 
-         if (geExtensionCustomCoordinates)
-             configParameters.put('buildScanPlugin.gradle-enterprise.extension.custom.coordinates', geExtensionCustomCoordinates)
+        if (geExtensionCustomCoordinates)
+            configParameters.put('buildScanPlugin.gradle-enterprise.extension.custom.coordinates', geExtensionCustomCoordinates)
 
-         if (ccudExtensionCustomCoordinates)
-             configParameters.put('buildScanPlugin.ccud.extension.custom.coordinates', ccudExtensionCustomCoordinates)
+        if (ccudExtensionCustomCoordinates)
+            configParameters.put('buildScanPlugin.ccud.extension.custom.coordinates', ccudExtensionCustomCoordinates)
 
-         if (allowUntrustedServer != null)
-             configParameters.put('buildScanPlugin.gradle-enterprise.allow-untrusted-server', allowUntrustedServer.toString())
+        if (allowUntrustedServer != null)
+            configParameters.put('buildScanPlugin.gradle-enterprise.allow-untrusted-server', allowUntrustedServer.toString())
 
-         if (commandLineBuildStepEnabled != null)
-             configParameters.put('buildScanPlugin.command-line-build-step.enabled', commandLineBuildStepEnabled.toString())
-     }
- }
+        if (commandLineBuildStepEnabled != null)
+            configParameters.put('buildScanPlugin.command-line-build-step.enabled', commandLineBuildStepEnabled.toString())
+    }
+}
