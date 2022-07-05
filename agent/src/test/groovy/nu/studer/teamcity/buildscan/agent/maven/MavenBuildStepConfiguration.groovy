3bc28b5528050858599472501fc01574124eb483
Clay Johnson <cjohnson@gradle.com>
2022-07-05 15:51:59 -0500

    Remove redundant properties from MavenBuildStepConfiguration

--- agent/src/test/groovy/nu/studer/teamcity/buildscan/agent/maven/MavenBuildStepConfiguration.groovy
+++ agent/src/test/groovy/nu/studer/teamcity/buildscan/agent/maven/MavenBuildStepConfiguration.groovy
@@ -4,17 +4,8 @@ final class MavenBuildStepConfiguration {
     String goals = 'clean package'
     String pathToPomFile = null
 
-    String geUrl = null
-    String geExtensionVersion = null
-    String ccudExtensionVersion = null
-    String geExtensionCustomCoordinates = null
-    String ccudExtensionCustomCoordinates = null
-    Boolean allowUntrustedServer = null
-    Boolean commandLineBuildStepEnabled = null
-
     Map<String, String> applyTo(Map<String, String> configParameters, Map<String, String> runnerParameters) {
         applyRunnerParameters(runnerParameters)
-        applyConfigParameters(configParameters)
 
         return runnerParameters
     }
@@ -24,28 +15,5 @@ final class MavenBuildStepConfiguration {
 
         if (pathToPomFile)
             runnerParameters.put('pomLocation', pathToPomFile)
-    }
-
-    private void applyConfigParameters(Map<String, String> configParameters) {
-        if (geUrl)
-            configParameters.put('buildScanPlugin.gradle-enterprise.url', geUrl)
-
-        if (geExtensionVersion)
-            configParameters.put('buildScanPlugin.gradle-enterprise.extension.version', geExtensionVersion)
-
-        if (ccudExtensionVersion)
-            configParameters.put('buildScanPlugin.ccud.extension.version', ccudExtensionVersion)
-
-        if (geExtensionCustomCoordinates)
-            configParameters.put('buildScanPlugin.gradle-enterprise.extension.custom.coordinates', geExtensionCustomCoordinates)
-
-        if (ccudExtensionCustomCoordinates)
-            configParameters.put('buildScanPlugin.ccud.extension.custom.coordinates', ccudExtensionCustomCoordinates)
-
-        if (allowUntrustedServer != null)
-            configParameters.put('buildScanPlugin.gradle-enterprise.allow-untrusted-server', allowUntrustedServer.toString())
-
-        if (commandLineBuildStepEnabled != null)
-            configParameters.put('buildScanPlugin.command-line-build-step.enabled', commandLineBuildStepEnabled.toString())
     }
 }
