5f5556191141961df24a98c4d6e84f33ebdda60e
Etienne Studer <etienne@studer.nu>
2022-08-19 11:12:42 +0200

    Change Result object creation a bit

--- agent/src/main/java/nu/studer/teamcity/buildscan/agent/MavenCommandExecutor.java
+++ agent/src/main/java/nu/studer/teamcity/buildscan/agent/MavenCommandExecutor.java
@@ -28,15 +28,14 @@ final class MavenCommandExecutor {
     Result execute(String args) throws IOException, InterruptedException {
         File mavenExec = getMvnExec();
         if (mavenExec == null) {
-            return new Result();
+            return Result.didNotExecute();
         }
 
         String command = mavenExec.getAbsolutePath() + " " + args;
         ProcessBuilder processBuilder = new ProcessBuilder(command.split(" ")).redirectErrorStream(true);
         Process process = processBuilder.start();
         process.waitFor();
-
-        return new Result(process);
+        return Result.forExecutedProcess(process);
     }
 
     @Nullable
@@ -55,38 +54,43 @@ final class MavenCommandExecutor {
 
     static class Result {
 
-        private final Process process;
+        private final boolean success;
+        @NotNull
+        private final String output;
 
-        Result() {
-            process = null;
+        private Result(boolean success, @NotNull String output) {
+            this.success = success;
+            this.output = output;
         }
 
-        Result(Process process) {
-            this.process = process;
+        static Result didNotExecute() {
+            return new Result(false, "");
+        }
+
+        static Result forExecutedProcess(Process process) {
+            String output = readOutput(process);
+            return new Result(process.exitValue() == 0, output);
         }
 
         boolean isSuccessful() {
-            if (process == null) {
-                return false;
-            }
-
-            return process.exitValue() == 0;
+            return success;
         }
 
         @NotNull
         String getOutput() {
-            if (process == null || !isSuccessful()) {
-                return "";
-            }
+            return output;
+        }
 
+        @NotNull
+        private static String readOutput(Process process) {
             StringBuilder sb = new StringBuilder();
+
+            // this logic eagerly consumes the entire output into memory, which should not be an issue when only
+            // used for `mvn --version`, which generates ~5 lines of output
+            // this should be revisited if other commands are executed here
 
             BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
             try {
-                // this logic eagerly consumes the entire output into memory, which should not be an issue when only
-                // used for `mvn --version`, which generates ~5 lines of output
-                // this should be revisited if other commands are executed here
-
                 int ch;
                 while ((ch = reader.read()) != -1) {
                     sb.append((char) ch);
@@ -97,7 +101,7 @@ final class MavenCommandExecutor {
                 try {
                     reader.close();
                 } catch (IOException e) {
-                    LOG.warn("Unable to close reader", e);
+                    LOG.warn("Unable to close reader of Maven process", e);
                 }
             }
 
