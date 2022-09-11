446e0d6fd5128c38733f5bef512f9110c4e1ea72
Etienne Studer <etienne@studer.nu>
2022-09-11 21:16:57 +0200

    Bump CCUD Gradle plugin to 1.8.1

--- agent/src/test/groovy/nu/studer/teamcity/buildscan/agent/gradle/BaseInitScriptTest.groovy
+++ agent/src/test/groovy/nu/studer/teamcity/buildscan/agent/gradle/BaseInitScriptTest.groovy
@@ -134,8 +134,8 @@ class BaseInitScriptTest extends Specification {
     }
 
     def declareGePluginAndCcudPluginApplication(GradleVersion gradleVersion) {
-        settingsFile << maybeAddPluginsToSettings(gradleVersion, '1.8')
-        buildFile << maybeAddPluginsToRootProject(gradleVersion, '1.8')
+        settingsFile << maybeAddPluginsToSettings(gradleVersion, '1.8.1')
+        buildFile << maybeAddPluginsToRootProject(gradleVersion, '1.8.1')
     }
 
     String maybeAddPluginsToSettings(GradleVersion gradleVersion, String ccudPluginVersion = null) {
