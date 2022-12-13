126cbbc0e42e6ab097168677818a539c247cce10
Clay Johnson <cjohnson@gradle.com>
2022-12-13 10:44:51 -0600

    Mark build scan data store when a build is started with a supported runner type

--- src/main/java/nu/studer/teamcity/buildscan/BuildScanBuildServerListener.java
+++ src/main/java/nu/studer/teamcity/buildscan/BuildScanBuildServerListener.java
@@ -7,15 +7,21 @@ import jetbrains.buildServer.web.openapi.PluginDescriptor;
 import org.apache.log4j.Logger;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Collection;
+
 public final class BuildScanBuildServerListener extends BuildServerAdapter {
 
     private static final Logger LOGGER = Logger.getLogger("jetbrains.buildServer.BUILDSCAN");
+    private static final String GRADLE_RUNNER = "gradle-runner";
+    private static final String MAVEN_RUNNER = "Maven2";
+    private static final String COMMAND_LINE_RUNNER = "simpleRunner";
 
     private final PluginDescriptor pluginDescriptor;
     private final SBuildServer buildServer;
     private final BuildScanDisplayArbiter buildScanDisplayArbiter;
     private final BuildScanLookup buildScanLookup;
     private final ExternalIntegration externalIntegration;
+    private final BuildScanDataStore buildScanDataStore;
 
     @SuppressWarnings("WeakerAccess")
     public BuildScanBuildServerListener(
@@ -23,13 +29,15 @@ public final class BuildScanBuildServerListener extends BuildServerAdapter {
         @NotNull SBuildServer buildServer,
         @NotNull BuildScanDisplayArbiter buildScanDisplayArbiter,
         @NotNull BuildScanLookup buildScanLookup,
-        @NotNull ExternalIntegration externalIntegration
+        @NotNull ExternalIntegration externalIntegration,
+        @NotNull BuildScanDataStore buildScanDataStore
     ) {
         this.pluginDescriptor = pluginDescriptor;
         this.buildServer = buildServer;
         this.buildScanDisplayArbiter = buildScanDisplayArbiter;
         this.buildScanLookup = buildScanLookup;
         this.externalIntegration = externalIntegration;
+        this.buildScanDataStore = buildScanDataStore;
     }
 
     @SuppressWarnings("unused")
@@ -42,6 +50,16 @@ public final class BuildScanBuildServerListener extends BuildServerAdapter {
     public void unregister() {
         buildServer.removeListener(this);
         LOGGER.info(String.format("Unregistered %s. %s-%s", getClass().getSimpleName(), pluginDescriptor.getPluginName(), pluginDescriptor.getPluginVersion()));
+    }
+
+    @Override
+    public void buildStarted(@NotNull SRunningBuild build) {
+        if (build.getBuildType() != null) {
+            Collection<String> runnerTypes = build.getBuildType().getRunnerTypes();
+            if (runnerTypes.contains(GRADLE_RUNNER) || runnerTypes.contains(MAVEN_RUNNER) || runnerTypes.contains(COMMAND_LINE_RUNNER)) {
+                buildScanDataStore.mark(build);
+            }
+        }
     }
 
     @Override
