7ae12710a29ca6b5fc2b5aed00fe49dd65b7ede3
Clay Johnson <cjohnson@gradle.com>
2023-03-07 14:54:00 -0600

    Update functional tests to cover Gradle 7.6.1

--- agent/src/test/groovy/nu/studer/teamcity/buildscan/agent/gradle/BaseInitScriptTest.groovy
+++ agent/src/test/groovy/nu/studer/teamcity/buildscan/agent/gradle/BaseInitScriptTest.groovy
@@ -38,7 +38,7 @@ class BaseInitScriptTest extends Specification {
     static final JdkCompatibleGradleVersion GRADLE_6_0 = new JdkCompatibleGradleVersion(GradleVersion.version('6.0.1'), 8, 13)
     static final JdkCompatibleGradleVersion GRADLE_6_9 = new JdkCompatibleGradleVersion(GradleVersion.version('6.9.4'), 8, 15)
     static final JdkCompatibleGradleVersion GRADLE_7_0 = new JdkCompatibleGradleVersion(GradleVersion.version('7.0.2'), 8, 16)
-    static final JdkCompatibleGradleVersion GRADLE_7_6 = new JdkCompatibleGradleVersion(GradleVersion.version('7.6'), 8, 19)
+    static final JdkCompatibleGradleVersion GRADLE_7_6 = new JdkCompatibleGradleVersion(GradleVersion.version('7.6.1'), 8, 19)
     static final JdkCompatibleGradleVersion GRADLE_8_0 = new JdkCompatibleGradleVersion(GradleVersion.version('8.0.2'), 8, 19)
 
     static final List<JdkCompatibleGradleVersion> GRADLE_VERSIONS_2_AND_HIGHER = [
