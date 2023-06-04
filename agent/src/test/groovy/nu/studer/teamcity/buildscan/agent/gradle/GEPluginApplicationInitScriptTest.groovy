1b69acdd97b2d37c3b34d9c29787626f7b80ecbd
Eric Haag <ehaag@gradle.com>
2023-06-04 11:42:47 -0500

    Bump Common Custom User Data Gradle plugin version from 1.10 to 1.11 (#256)

--- agent/src/test/groovy/nu/studer/teamcity/buildscan/agent/gradle/GEPluginApplicationInitScriptTest.groovy
+++ agent/src/test/groovy/nu/studer/teamcity/buildscan/agent/gradle/GEPluginApplicationInitScriptTest.groovy
@@ -9,7 +9,7 @@ import static org.junit.Assume.assumeTrue
 class GEPluginApplicationInitScriptTest extends BaseInitScriptTest {
 
     private static final String GE_PLUGIN_VERSION = '3.13.3'
-    private static final String CCUD_PLUGIN_VERSION = '1.10'
+    private static final String CCUD_PLUGIN_VERSION = '1.11'
 
     private static final GradleVersion GRADLE_6 = GradleVersion.version('6.0')
 
