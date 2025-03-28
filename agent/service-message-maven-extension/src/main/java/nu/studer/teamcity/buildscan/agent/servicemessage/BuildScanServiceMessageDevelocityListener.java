944428ceed182f1ed0351307ec536acd53ff7944
daz <daz@gradle.com>
2025-03-28 16:31:33 -0600

    Prevent multiple invocations of DevelocityListener

--- agent/service-message-maven-extension/src/main/java/nu/studer/teamcity/buildscan/agent/servicemessage/BuildScanServiceMessageDevelocityListener.java
+++ agent/service-message-maven-extension/src/main/java/nu/studer/teamcity/buildscan/agent/servicemessage/BuildScanServiceMessageDevelocityListener.java
@@ -6,6 +6,8 @@ import com.gradle.develocity.agent.maven.api.DevelocityListener;
 import org.apache.maven.execution.MavenSession;
 import org.codehaus.plexus.component.annotations.Component;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+
 @SuppressWarnings("unused")
 @Component(
     role = DevelocityListener.class,
@@ -13,9 +15,14 @@ import org.codehaus.plexus.component.annotations.Component;
     description = "Interacts with Maven builds invoked from TeamCity"
 )
 public final class BuildScanServiceMessageDevelocityListener extends BuildScanServiceMessageListener implements DevelocityListener {
+    private static final AtomicBoolean hasBeenCalled = new AtomicBoolean(false);
 
     @Override
     public void configure(DevelocityApi api, MavenSession session) {
+        if (hasBeenCalled.getAndSet(true)) {
+            return; // Ignore subsequent calls
+        }
+
         super.configure(new DevelocityApiAdapter(api), session);
     }
 
