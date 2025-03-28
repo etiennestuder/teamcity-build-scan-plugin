519efb001fc111b2ddf9b3b8779e40282c5cb894
daz <daz@gradle.com>
2025-03-28 16:31:33 -0600

    Prevent multiple invocations of extension listeners
    A bug in the develocity-maven-extension:1.24.2 is causing the listener to be constructed and initialized twice.
    
    This static guard will ensure singleton semantics for `DevelocityListener` and `GradleEntepriseListener`.

--- agent/service-message-maven-extension/src/main/java/nu/studer/teamcity/buildscan/agent/servicemessage/BuildScanServiceMessageListener.java
+++ agent/service-message-maven-extension/src/main/java/nu/studer/teamcity/buildscan/agent/servicemessage/BuildScanServiceMessageListener.java
@@ -7,12 +7,20 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 abstract class BuildScanServiceMessageListener {
 
+    private static final AtomicBoolean hasBeenCalled = new AtomicBoolean(false);
     private final Logger logger = LoggerFactory.getLogger(BuildScanServiceMessageListener.class);
 
     protected void configure(DevelocityAdapter develocity, MavenSession session) {
+        // A bug in DV extension 1.23.1 can result in duplicate listener calls.
+        // This guard will ensure that we only capture the build scan link once per Maven execution.
+        if (hasBeenCalled.getAndSet(true)) {
+            return; // Ignore subsequent calls
+        }
+
         logger.debug("Executing extension: " + getClass().getSimpleName());
 
         // When a Maven build step has an explicitly defined pom.xml file location, TeamCity will run an info goal on it. Ignore build scan publication in this case.
